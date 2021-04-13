c0058c4cc3317641ee3045175f48d4fa56fee1f7
PWhitehat <77223407+PWhitehat@users.noreply.github.com>
2021-04-13 18:13:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,9 +4,10 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 var starImg, fairyImg, bgImg;
-var fairy , fairyVoice;
+var fairy, fairyBody, fairy_options, fairyVoice;
 var star, starBody;
 var world, engine;
+var follow = 0;
 
 function preload()
 {
@@ -20,8 +21,6 @@ function preload()
 function setup() {
 	createCanvas(800, 750);
 
-	//fairyVoice.play();
-
 	fairy = createSprite(130, 520);
 	fairy.addImage("fairyflying",fairyImg);  
 	fairy.scale =0.25;
@@ -33,19 +32,29 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	star_options = {isStatic: false}
+	fairy_options = {isStatic: true}
+	fairyBody = Bodies.rectangle(500, 520, 350, 20, fairy_options);
+	World.add(world, fairyBody);
 
+	fairyVoice.play();
+
+}
+
+function makeStar() {
+
+	star_options = {isStatic: false}
 	starBody = Bodies.circle(650 , 30 , 5 , star_options);
 	World.add(world, starBody);
-	
+
 }
 
 function keyPressed() {
 
 	if (keyCode === 32) {
 
-		
-
+		makeStar();
+		follow = 1;
+	
 	}
 
 }
@@ -54,9 +63,23 @@ function draw() {
   background(bgImg);
 
   Engine.update(engine);
-  
+
+
+  //if (cat.x - mouse.x < (cat.width - mouse.width) / 2) {
+
+
+
+  //}
+
+if (follow === 1) {
+
   star.x = starBody.position.x;
-  star.y = starBody.position.y;
+  star.y = starBody.position.y;  
+
+}
+
+  fairy.x = fairyBody.position.x;
+  fairy.y = fairyBody.position.y;  
 
   drawSprites();
 
